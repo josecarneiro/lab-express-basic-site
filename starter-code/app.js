b1246fe16e9b92ade3baa87a020bb29cb84b2206
mloureiro93 <mariamadureiraloureiro@gmail.com>
2019-11-12 14:15:09 +0000

    sam page

---
+++ starter-code/app.js
@@ -0,0 +1,38 @@
+const express = require("express")
+
+const app = express();
+
+app.use(express.static('public'));
+
+//Route Handlers
+app.get('/', (request, response, next) => {
+    
+    response.sendFile(__dirname + '/views/index.html')
+});
+
+app.get('/about', (request, response, next) => {
+    response.sendFile(__dirname + '/views/about.html')
+
+});
+
+app.get('/works', (request, response, next) => {
+    response.sendFile(__dirname + '/views/works.html')
+
+});
+
+app.get('/gallery', (request, response, next) => {
+    response.sendFile(__dirname + '/views/gallery.html')
+
+});
+
+//Catch-all Route Handler
+
+app.get('*', (request, response, next) => {
+    response.status(404);
+    response.sendFile(__dirname + '/views/error.html')
+
+});
+
+
+
+app.listen(3100);
